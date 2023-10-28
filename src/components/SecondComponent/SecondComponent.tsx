43c4f5019b25aaf3ab742590f74fac4f16b0717d
Krutikaa04 <waghkrutika01@gmail.com>
2023-10-28 15:05:17 +0530

    added new files

---
+++ src/components/SecondComponent/SecondComponent.tsx
@@ -0,0 +1,6 @@
+export function SecondComponent():JSX.Element{
+    return <img
+    src="https://img.freepik.com/free-photo/purple-osteospermum-daisy-flower_1373-16.jpg"
+    alt="A flower"
+    />
+}
